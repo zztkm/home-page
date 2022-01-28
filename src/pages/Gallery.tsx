706c12fcf8d9ea2a3df49916848e2e6a7c30e087
zztkm <takumi.basket1682@gmail.com>
2022-01-28 15:44:33 +0900

    まじで頼んだ

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -2,8 +2,8 @@ import { ReactElement } from "react";
 import Head from "../components/Head";
 import "98.css"
 
-import hyouBgWaterBlue from '/hyou-1-bg-waterblue.jpg'
-import kumoriSuisen from '/kumori-suisen.png'
+import hyouBgWaterBlue from '../assets/hyou-1-bg-waterblue.jpg'
+import kumoriSuisen from '../assets/kumori-suisen.png'
 
 export default function Gallery(): ReactElement {
 	return (
